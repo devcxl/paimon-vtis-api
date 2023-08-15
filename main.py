05363522b13bf75572d6ed3ddf7c56522b40dab6
devcxl <orange.cxl@qq.com>
2023-08-15 22:31:44 +0800

    install commit

---
+++ main.py
@@ -0,0 +1,89 @@
+
+import soundfile as sf
+import torch
+from fastapi import FastAPI, Request, HTTPException, Header
+from fastapi.responses import FileResponse,StreamingResponse
+import hashlib
+import torch
+from storage import Model, SQLiteDB, VitsHistory, User
+import utils
+import commons
+from models import SynthesizerTrn
+from text.symbols import symbols
+from text import text_to_sequence
+import time
+from typing import Annotated
+
+
+def get_text(text, hps):
+    text_norm = text_to_sequence(text, hps.data.text_cleaners)
+    if hps.data.add_blank:
+        text_norm = commons.intersperse(text_norm, 0)
+    text_norm = torch.LongTensor(text_norm).cpu()
+    return text_norm
+
+
+def calculate_md5(text):
+    md5_hash = hashlib.md5(text.encode())
+    return md5_hash.hexdigest()
+
+
+hps = utils.get_hparams_from_file("biaobei_base.json")
+
+net_g = SynthesizerTrn(
+    len(symbols),
+    hps.data.filter_length // 2 + 1,
+    hps.train.segment_size // hps.data.hop_length,
+    **hps.model).cpu()
+_ = net_g.eval()
+
+_ = utils.load_checkpoint('/save/vits/models/G_1434000.pth', net_g, None)
+
+
+db = SQLiteDB('new.sqlite')
+Model.initialize(db)
+try:
+    VitsHistory.create_table()
+    User.create_table()
+
+    insert=User()
+    insert.access_token = 'fcd30694b0184b68af2e1cd38f5245db'
+    insert.password = 'fcd30694b0184b68af2e1cd38f5245db'
+    insert.save()
+
+except Exception as e:
+    pass
+
+app = FastAPI()
+
+
+@app.middleware("http")
+async def add_process_time_header(request: Request, call_next):
+    start_time = time.time()
+    response = await call_next(request)
+    process_time = time.time() - start_time
+    response.headers["X-Process-Time"] = str(process_time)
+    return response
+
+
+@app.get("/api/paimon")
+async def paimon(content: str = '你好，我是派蒙。', speed: float = 1.0, access_token: Annotated[str | None, Header()] = None):
+    if len(content) > 300:
+        raise HTTPException(status_code=401, detail="Content too long")
+    user = User.fetch_one('access_token=?', (access_token,))
+    if user is not None:
+        history = VitsHistory()
+        history.user_id = user.id
+        history.content = content
+        history.save()
+        audio_path ='paimon.wav'
+        stn_tst = get_text(content, hps)
+        with torch.no_grad():
+            x_tst = stn_tst.cpu().unsqueeze(0)
+            x_tst_lengths = torch.LongTensor([stn_tst.size(0)]).cpu()
+            audio = net_g.infer(x_tst, x_tst_lengths, noise_scale=.667, noise_scale_w=0.8,
+                                length_scale=speed)[0][0, 0].data.cpu().float().numpy()
+        sf.write(audio_path, audio, samplerate=hps.data.sampling_rate)
+        return FileResponse(audio_path)
+    else:
+        raise HTTPException(status_code=401, detail="access_token error")
---
+++ storage.py
@@ -0,0 +1,140 @@
+import sqlite3
+
+class SQLiteDB:
+    def __init__(self, db_name):
+        self.conn = sqlite3.connect(db_name)
+        self.conn.row_factory = sqlite3.Row
+        self.cursor = self.conn.cursor()
+
+    def execute(self, query, params=None):
+        if params:
+            self.cursor.execute(query, params)
+        else:
+            self.cursor.execute(query)
+        self.conn.commit()
+
+    def fetch_all(self, query, params=None):
+        if params:
+            self.cursor.execute(query, params)
+        else:
+            self.cursor.execute(query)
+        return self.cursor.fetchall()
+
+    def fetch_one(self, query, params=None):
+        if params:
+            self.cursor.execute(query, params)
+        else:
+            self.cursor.execute(query)
+        return self.cursor.fetchone()
+
+    def total(self, query, params=None):
+        if params:
+            result = self.fetch_one(query, params)
+        else:
+            result = self.fetch_one(query)
+        return result[0] if result else 0
+
+    def delete(self, query, params=None):
+        if params:
+            self.execute(query, params)
+        else:
+            self.execute(query)
+
+
+class Model:
+    db = None
+
+    @classmethod
+    def initialize(cls, db):
+        cls.db = db
+
+    @classmethod
+    def create_table(cls):
+        columns = ', '.join([f"{column} {data_type}" for column,
+                            data_type in cls.__dict__.items() if not column.startswith('__')])
+        create_table_query = f"CREATE TABLE IF NOT EXISTS {cls.__name__} (id INTEGER PRIMARY KEY AUTOINCREMENT, {columns},create_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,update_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP)"
+        
+        cls.db.execute(create_table_query)
+
+    def save(self):
+        columns = ', '.join(self.__dict__.keys())
+        placeholders = ', '.join(['?' for _ in self.__dict__.values()])
+        insert_query = f"INSERT INTO {self.__class__.__name__} ({columns}) VALUES ({placeholders})"
+        values = tuple(self.__dict__.values())
+        self.db.execute(insert_query, values)
+
+    def update(self):
+        update_query = f"UPDATE {self.__class__.__name__} SET " + ', '.join(
+            [f"{column} = ?" for column in self.__dict__.keys() if column != 'id']) + " WHERE id = ?"
+        values = tuple(value for column, value in self.__dict__.items()
+                       if column != 'id') + (self.id,)
+        self.db.execute(update_query, values)
+
+    def save_or_update(self):
+        if hasattr(self, 'id') and self.id:
+            self.update()
+        else:
+            self.save()
+
+    def delete(self):
+        delete_query = f"DELETE FROM {self.__class__.__name__} WHERE id=?"
+        self.db.delete(delete_query, (self.id,))
+
+    @classmethod
+    def fetch_one(cls, where='1=1', params=None):
+        sql = f"""SELECT * FROM {cls.__name__} WHERE {where}"""
+        if params:
+            result = cls.db.fetch_one(sql, params)
+        else:
+            result = cls.db.fetch_one(sql)
+
+        if result:
+            instance = cls()
+            instance.__dict__.update(result)
+            return instance
+        else:
+            return None
+
+    @classmethod
+    def total(cls, where='1=1', params=None):
+        sql = f"""SELECT count(1) FROM {cls.__name__} WHERE {where}"""
+        return cls.db.total(sql)
+
+    @classmethod
+    def fetch_all(cls, where='1=1', params=None):
+        sql = f"""SELECT * FROM {cls.__name__} WHERE {where}"""
+        if params:
+            results = cls.db.fetch_all(sql, params)
+        else:
+            results = cls.db.fetch_all(sql)
+
+        instances = []
+        for result in results:
+            instance = cls()
+            instance.__dict__.update(result)
+            instances.append(instance)
+
+        return instances
+
+
+class VitsHistory(Model):
+    user_id = "INTEGER"
+    content = "TEXT"
+
+class User(Model):
+    access_token = "TEXT"
+    password = "TEXT"
+
+
+if __name__ == "__main__":
+
+    db = SQLiteDB('new.sqlite')
+    Model.initialize(db)
+    # Message.create_table()
+    # flag = Message.fetch_one("msg_id = '123456'")
+    # message = Message()
+    # message.context = 'msg.text'
+    # message.message_id = 'msg.MsgId'
+    # message.toUserName = 'msg.NickName or ''ME'''
+    # message.fromUserName = 'msg.ActualNickName or msg.user.NickName'
+    # message.save()
